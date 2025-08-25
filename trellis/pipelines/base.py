6fa9a9e1b5de9b1b454278a5ddb3a027b76a4a4a
Ahmed Mansour <ahmed@loom.li>
2025-08-25 21:29:52 +0100

    models: fix model loading

--- trellis/pipelines/base.py
+++ trellis/pipelines/base.py
@@ -38,10 +38,8 @@ class Pipeline:
 
         _models = {}
         for k, v in args['models'].items():
-            try:
-                _models[k] = models.from_pretrained(f"{path}/{v}")
-            except:
-                _models[k] = models.from_pretrained(v)
+            # Always use the full path within the repository
+            _models[k] = models.from_pretrained(f"{path}/{v}")
 
         new_pipeline = Pipeline(_models)
         new_pipeline._pretrained_args = args
